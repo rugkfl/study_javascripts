8d1eb687734b4aa09f1eeb582e2ea56da4e0e1c7
rugkfl <kimgyoungha61@gmail.com>
2024-01-24 16:05:53 +0900

    update

---
+++ docs/browsers.js/js/loops.js
@@ -0,0 +1,19 @@
+// learning after object
+const animals_array = [
+    { name: "dog", species: "canine" },
+    { name: "cat", species: "feline" },
+    { name: "bird", species: "avian" },
+    { name: "fish", species: "aquatic" },
+    { name: "lizard", species: "reptile" },
+  ];
+
+  
+  let animal_list = "";
+  for (let animal_object of animals_array){
+    animal_list = `${animal_list}<tr>
+    <td>${animal_object["name"]}</td>
+    <td>${animal_object["species"]}</td></tr>`;
+  }
+
+  let aniamls_element = document.querySelector("#animals_array_id"); 
+  aniamls_element.innerHTML = animal_list;
